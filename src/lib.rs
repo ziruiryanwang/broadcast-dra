a76dc083c3e3de6c1fcc85875995a4ab5072c387
ziruiryanwang <ryan1330@foxmail.com>
2024-11-25 13:39:00 +0000

    chore: re-export transcript types

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ pub mod collateral;
 pub mod auction;
 pub mod simulation;
 
-pub use auction::{AuctionOutcome, FalseBid, PublicBroadcastDRA};
+pub use auction::{AuctionOutcome, FalseBid, PublicBroadcastDRA, Transcript, CommitmentEvent, RevealEvent};
 pub use collateral::collateral_requirement;
 pub use commitment::{
     Commitment, CommitmentScheme, NonMalleableShaCommitment, PedersenRistrettoCommitment,
